411dc27139a0bc009061749d53062f86a0ce5729
HiImLawtSimp1e <nqh1703k2@gmail.com>
2023-11-15 18:23:30 +0700

    create Components

--- RadzenBook.Client/Program.cs
+++ RadzenBook.Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -8,6 +9,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
