f0775f73908161693bd1a6a30d4599ec9fba6324
Trần văn quý Trần văn quý <tquy2203@gmail.com>
2023-11-23 13:46:43 +0700

    layout mvc

---
+++ RadzenBook.Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadzenBook.Dashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RadzenBook.Dashboard/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadzenBook.Dashboard.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
