f37f0b33bfba1f82e77222e6643bc936b3774dc7
Cien <mr.zenner203@gmail.com>
2024-01-06 18:01:01 +0700

    Dashboard

---
+++ RadzenBook.Dashboard/Data/DTO/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RadzenBook.Dashboard.Data.DTO
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- RadzenBook.Dashboard/Program.cs
+++ RadzenBook.Dashboard/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]) });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
