bd3dd59e6256db252f69dc0c4d5c55ac32bfacd0
kienct <kingofdespair69@gmail.com>
2024-01-15 00:23:36 +0700

    add l10n test

--- src/Server/RadzenBook.Infrastructure/Localization/PoFileLocationProvider.cs
+++ src/Server/RadzenBook.Infrastructure/Localization/PoFileLocationProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using OrchardCore.Localization;
 
 namespace RadzenBook.Infrastructure.Localization;
@@ -7,16 +8,20 @@ namespace RadzenBook.Infrastructure.Localization;
 public class PoFileLocationProvider : ILocalizationFileLocationProvider
 {
     private readonly IFileProvider _fileProvider;
+    private readonly string _resourcesContainer;
 
-    public PoFileLocationProvider(IHostEnvironment hostingEnvironment)
+    public PoFileLocationProvider(IHostEnvironment hostingEnvironment,
+        IOptions<LocalizationOptions> localizationOptions)
     {
         _fileProvider = hostingEnvironment.ContentRootFileProvider;
+        _resourcesContainer = localizationOptions.Value.ResourcesPath;
     }
 
     public IEnumerable<IFileInfo> GetLocations(string cultureName)
     {
         // Loads all *.po files from the culture folder under the Resource Path.
         // For example: /Localization/en-US/*.po
-        return _fileProvider.GetDirectoryContents(Path.Combine("Localization", cultureName));
+        var result = _fileProvider.GetDirectoryContents(Path.Combine(_resourcesContainer, cultureName));
+        return result.Where(x => x.Name.EndsWith(".po"));
     }
 }
